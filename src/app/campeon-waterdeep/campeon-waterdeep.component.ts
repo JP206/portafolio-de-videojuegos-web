8530ce09803aec6ff028edf2e6f29ea68f616941
JP206 <88661539+JP206@users.noreply.github.com>
2023-01-12 20:17:08 -0300

    final

--- src/app/campeon-waterdeep/campeon-waterdeep.component.ts
+++ src/app/campeon-waterdeep/campeon-waterdeep.component.ts
@@ -10,7 +10,7 @@ export class CampeonWaterdeepComponent implements OnInit {
 
   constructor(config: NgbCarouselConfig) {
 		// customize default values of carousels used by this component tree
-		config.interval = 6000;
+		config.interval = 8000;
 		config.wrap = true;
 		config.keyboard = false;
 		config.pauseOnHover = true;
